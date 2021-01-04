683b2e69f1570b946f8e8e3e53aa2f0e1dfb84c8
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-03 23:41:34 -0600

    set up both Discussions and Comments controllers

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :set_discussion
+
+  def index
+    @comments = @discussion.comments
+    render json: @comments
+  end
+
+  def create
+    @comment = @discussion.comments.new(comment_params)
+    if @comment.save
+      render json: @comment
+    else
+      render jason: { errors: @comment.errors }
+    end
+  end
+
+  def show
+    @comment = @discussion.comments.find_by(id: params[:id])
+    render jason: @comment
+  end
+
+  def destroy
+    @comment = @discussion.comments.find_by(id: params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(comment).permit(:username, :content, :discussion_id)
+  end
+
+  def set_discussion
+    @discussion = Discussion.find(params[:discussion_id])
+  end
+
 end
--- app/controllers/api/v1/discussions_controller.rb
+++ app/controllers/api/v1/discussions_controller.rb
@@ -1,2 +1,33 @@
 class Api:V1::DiscussionsController < ApplicationController
+
+  def index
+    @discussions = Discussion.all
+    render json: @discussions
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+    if @discussion.save
+      render json: @discussion
+    else
+      render jason: { errors: @discussion.errors }
+    end
+  end
+
+  def show
+    @discussion = Discussion.find(params[:id])
+    render jason: @discussion
+  end
+
+  def destroy
+    @discussion = Discussion.find(params[:id])
+    @discussion.destroy
+  end
+
+  private
+
+  def discussion_params
+    params.require(discussion).permit(:topic)
+  end
+
 end
