9e4db93f29d4d77e004cce1c027eb4cb6f37085e
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-04 21:08:40 -0600

    add pry gem and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Discussion.create!(
+  [
+    {topic: 'What are the some of the pros and cons of social media?'},
+    {topic: 'Should so many year of experience in a certain field count as a degree?'},
+  ]
+)
+
+Comment.create!(
+  [
+    {discussion_id: 1, username: 'randomUser123', content: 'I think social media help people around the world connect, but it creates disconnection from those who are physically close to us.'},
+
+    {discussion_id: 1, username: 'user2', content: 'I dont think it necesseraly has that effect on all of us randomUser123'},
+  ]
+)
