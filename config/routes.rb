61f2ba0924322de4c6d2bc7035b42dcd54496ac3
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-14 13:19:49 -0600

    update routes -add user resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :discussions do
-        resources :comments
+        resources :users do
+          resources :comments
+        end
       end
     end
   end
