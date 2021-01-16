f8a7f5a4d95dace870ba9531e330db5ae4e5814b
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-16 13:10:13 -0600

    add show, create, and index controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
-class UsersController < ApplicationController
+  def index
+    @users = @discussion.users
+    render json: @users
+  end
+
+  def create
+    @user = @discussion.users.new(user_params)
+    if @user.save
+      render json: @discussion
+    else
+      render json: { errors: @user.errors }
+    end
+  end
+
+   def show
+      @user = @discussion.users.find_by(id: params[:id])
+      render json: @user
+   end
 end
