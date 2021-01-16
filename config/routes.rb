4fceb5e299a242d76f3a76f6df60730745f2dcc3
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-16 13:11:46 -0600

    update routes to only have the needed controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :discussions do
-        resources :users do
-          resources :comments
+      resources :discussions, only: [:index, :create, :show] do
+        resources :users, only: [:index, :create] do
+          resources :comments, only: [:index, :create]
         end
       end
     end
