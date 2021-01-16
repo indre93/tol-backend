4be4db5c2bd0a59390bc37360d6864bbd7cbbfc0
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-16 12:58:08 -0600

    add scope to uniqueness validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :discussion
   has_many :comments
   validates :username, presence: true
-  validates :username, uniqueness: true
+  validates_uniqueness_of :username, scope: :discussion_id
 end
