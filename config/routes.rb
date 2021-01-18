c5595e7e633e33689034b0ee521aea26dfd20da5
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-17 18:30:16 -0600

    remove user resource route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :discussions, only: [:index, :create, :show] do
-        resources :users, only: [:index, :create] do
-          resources :comments, only: [:index, :create]
-        end
+        resources :comments, only: [:index, :create]
       end
     end
   end
