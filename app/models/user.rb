83a2eb31ef432c71c99fb9280400d9755741ef4d
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-17 18:27:40 -0600

    remove user resource

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  belongs_to :discussion
-  has_many :comments
-  validates :username, presence: true
-  validates_uniqueness_of :username, scope: :discussion_id
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,4 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
-  belongs_to :discussion
-end
