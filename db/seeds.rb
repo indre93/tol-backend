42ac9f811ff56eb5a5d914518ec3ad20a3e38cf4
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-18 16:37:56 -0600

    add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,18 @@ Discussion.create!(
   [
     {topic: "What are the some of the pros and cons of social media?"},
     {topic: "Should so many year of experience in a certain field count as a degree?"},
+    {topic: "Exercitationem vel eveniet in."},
+    {topic: "Sunt ut eos eos deserunt corporis accusamus et eum."},
   ]
 )
 
 Comment.create!(
   [
     {discussion_id: 1, username: "randomUser123", content: "I think social media help people around the world connect, but it creates disconnection from those who are physically close to us."},
-    {discussion_id: 1,  username: "user2", content: "I dont think it necesseraly has that effect on all of us randomUser123"},
+    {discussion_id: 1,  username: "user2", content: "I dont think it necesseraly has that effect on all of us"},
+    {discussion_id: 1, username: "tempore", content: "Aut omnis sed repellendus fugit quod aut. Sit atque quas. Sit aut voluptas quod sit. Ut eos voluptatem dolores consequuntur provident eos animi sunt. Aut accusantium iusto in consequatur consequatur officiis animi quisquam dolorum. Ea sint voluptatem dolor."},
+    {discussion_id: 1, username: "quos", content: "Eum ipsa quidem rem. Cum architecto recusandae veritatis blanditiis ut molestias et expedita necessitatibus. Quia dolor inventore."},
+    {discussion_id: 1, username: "fugiat", content: "dolorem adipisci minima"},
+    {discussion_id: 1, username: "ipsum", content: "Et rerum quaerat consequatur minus sint."},
   ]
 )
