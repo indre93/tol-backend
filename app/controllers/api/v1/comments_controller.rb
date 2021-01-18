828dfa853d0aad7d71e316b9a705e6a8e1b59ce6
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-17 18:29:20 -0600

    replace user w discussion

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::CommentsController < ApplicationController
-  before_action :set_user
+  before_action :set_discussion
 
   def index
-    @comments = @user.comments
+    @comments = @discussion.comments
     render json: @comments
   end
 
   def create
-    @comment = @user.comments.new(comment_params)
+    @comment = @discussion.comments.new(comment_params)
     if @comment.save
       render json: @discussion
     else
@@ -18,11 +18,11 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :discussion_id)
+    params.require(:comment).permit(:username, :content :discussion_id)
   end
 
-  def set_user
-    @user = User.find(params[:user_id])
+  def set_discussion
+    @discussion = Discussion.find(params[:discussion_id])
   end
 
 end
