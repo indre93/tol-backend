cb7663de2551c53d9f954efe2eb6515c176480bd
Indre Bakeer <indre.bakeer@gmail.com>
2021-01-20 17:23:40 -0600

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-
 Discussion.create!(
   [
-    {topic: "What are the some of the pros and cons of social media?"},
-    {topic: "Should so many year of experience in a certain field count as a degree?"},
+    {topic: "Aut et aut qui. Dicta sit omnis molestiae nostrum expedita nihil dignissimos."},
+    {topic: "Pariatur consequatur saepe ad perspiciatis."},
     {topic: "Exercitationem vel eveniet in."},
     {topic: "Sunt ut eos eos deserunt corporis accusamus et eum."},
   ]
@@ -10,8 +9,8 @@ Discussion.create!(
 
 Comment.create!(
   [
-    {discussion_id: 1, username: "randomUser123", content: "I think social media help people around the world connect, but it creates disconnection from those who are physically close to us."},
-    {discussion_id: 1,  username: "user2", content: "I dont think it necesseraly has that effect on all of us"},
+    {discussion_id: 1, username: "randomUser123", content: "Commodi minus voluptas accusantium."},
+    {discussion_id: 1,  username: "user2", content: "Omnis veniam qui dolores eum id quia nostrum. Dicta voluptatem enim non. Harum non rerum et ut id. Sed in vel ipsa quibusdam nisi iure."},
     {discussion_id: 1, username: "tempore", content: "Aut omnis sed repellendus fugit quod aut. Sit atque quas. Sit aut voluptas quod sit. Ut eos voluptatem dolores consequuntur provident eos animi sunt. Aut accusantium iusto in consequatur consequatur officiis animi quisquam dolorum. Ea sint voluptatem dolor."},
     {discussion_id: 1, username: "quos", content: "Eum ipsa quidem rem. Cum architecto recusandae veritatis blanditiis ut molestias et expedita necessitatibus. Quia dolor inventore."},
     {discussion_id: 1, username: "fugiat", content: "dolorem adipisci minima"},
